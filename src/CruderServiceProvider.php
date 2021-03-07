e4835d7217e55772c5325b44140af215c710070f
akcauser <akcaertugrulgazi@gmail.com>
2021-03-07 21:15:46 +0300

    example page created

--- src/CruderServiceProvider.php
+++ src/CruderServiceProvider.php
@@ -8,6 +8,9 @@ class CruderServiceProvider extends ServiceProvider {
 
     public function boot()
     {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'cruder');
+
         $this->publishes([
             __DIR__. '/../config/cruder.php' => config_path('cruder.php'),
         ]);
